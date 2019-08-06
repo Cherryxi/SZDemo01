ca177ccc77c4efcab0fc09d3674d48484e481427
shycie <shycie.zuo@r2games.com>
2019-08-06 11:28:22 +0800

    修改控制器的背景颜色为蓝色

--- FlowerSweet/ViewController.swift
+++ FlowerSweet/ViewController.swift
@@ -17,6 +17,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("ti si")
+        self.view.backgroundColor = UIColor.blue
         // Do any additional setup after loading the view, typically from a nib.
     }
 
